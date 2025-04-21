0d04fc06fac97d0829f2b1b8cf7fde616fe5b940
Philip Hill <hill.philip@gmail.com>
2025-04-21 23:56:57 +0100

    (fix) If your app runner supports health_check_configuration - them you had better support it - the fix implements the missing URL

--- milestone01/backend/python/main.py
+++ milestone01/backend/python/main.py
@@ -5,6 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message":"You've found the home page!"}
+    return {"message":"You've found Phils home page!"}
 
-
+@app.get("/health")
+async def health():
+    return {"status":"Alive!"}
