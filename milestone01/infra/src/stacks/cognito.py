8ff8ebf1ad7cf9beb007d2542359a605adefc368
Philip Hill <philip.hill@hidglobal.com>
2025-04-23 13:08:09 +0100

    (feat) Signup email trigger - validate known email domains.

--- milestone01/infra/src/stacks/cognito.py
+++ milestone01/infra/src/stacks/cognito.py
@@ -8,23 +8,26 @@ from aws_cdk import aws_lambda_python_alpha as lambda_python
 
 from constructs import Construct
 
+# Setup  
+#
 class CognitoStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Get restrict domain function name from config
-        restrict_function_name = "lambda-restrict-domain"
-        restrict_function_path = Path(os.getcwd()).parent.joinpath("lambda/validate_email_domain")
+        validate_email_function_name = "lambda-restrict-domain"
+        validate_email_function_path = Path(os.getcwd()).parent.joinpath("lambda/validate_email_domain")
 
-        print("restrictlambda path at " + str(restrict_function_path))
+        
+        print("restrictlambda path at " + str(validate_email_function_name))
         domain_restrict_lambda = lambda_python.PythonFunction(
             self,
-            restrict_function_name,
+            validate_email_function_name,
             description="Function is to restrict external users to user pool; allow only hidglobal/assaabloy/gmail users.",
-            runtime=_lambda.Runtime.PYTHON_3_12,
+            runtime=_lambda.Runtime.PYTHON_3_13,
             handler="lambda_handler",
             index="main.py",
-            entry=str(restrict_function_path),
+            entry=str(validate_email_function_path),
         )
 
         # Grant Cognito permission to invoke the Lambda function
@@ -33,7 +36,6 @@ class CognitoStack(Stack):
             principal=iam.ServicePrincipal("cognito-idp.amazonaws.com"),
             action="lambda:InvokeFunction",
         )
-
 
         user_pool_name = "milestone1_userpool"
 
@@ -57,10 +59,14 @@ class CognitoStack(Stack):
             account_recovery=cognito.AccountRecovery.EMAIL_ONLY,
             self_sign_up_enabled=True,
             removal_policy=RemovalPolicy.DESTROY,
-            #lambda_triggers=cognito.UserPoolTriggers(     # <- this to add a trigger
-            #    pre_sign_up=domain_restrict_lambda
-            #),
+            lambda_triggers=cognito.UserPoolTriggers(     # <- this to add a trigger
+                pre_sign_up=domain_restrict_lambda,
+            ),
         )
+
+        # // or attach the Lambda as a trigger to the User Pool after creation
+        # userPool.AddTrigger(UserPoolOperation.PRE_SIGN_UP, domain_restrict_lambda);
+
 
         user_pool_client = user_pool.add_client(
             "main-client",
@@ -70,6 +76,8 @@ class CognitoStack(Stack):
             ),
         )
 
+        # CfnOutput - CloudFormationOutput export objects for other stacks
+        # Other stacks can import with Import with aws_cdk.Fn.importValue('<name>')) 
         _ = CfnOutput(
             self,
             "user_pool_id",
--- milestone01/lambda/validate_email_domain/main.py
+++ milestone01/lambda/validate_email_domain/main.py
@@ -1,14 +1,30 @@
+# See also https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-lambda-pre-sign-up.html
+
+
 def lambda_handler(event, context):
     # It sets the user pool autoConfirmUser flag after validating the email domain
     event['response']['autoConfirmUser'] = False
-
+  
+    print(event)
+    
     # Split the email address so we can compare domains
     address = event['request']['userAttributes']['email'].split('@')
+    
+    print("Got address " + address[1])
 
-    # This example uses a custom attribute 'custom:domain'
-    if 'custom:domain' in event['request']['userAttributes']:
-        if event['request']['userAttributes']['custom:domain'] == address[1]:
-            event['response']['autoConfirmUser'] = True
+    if (address[1] == "gmail.com") or (address[1] == "hidglobal.com"):
+        print("Domain Found")
+
+        # skip verification for known domains
+        event['response']['autoConfirmUser'] = True
+        event['response']['autoVerifyEmail'] = True
+        
+    else:
+        # unknow, invalid? dont allow them to register
+        print("Domain NOT found - exception")
+        raise Exception("AccessDenied: Email domain is not allowed")
+    
+    print(event)
 
     # Return to Amazon Cognito
     return event
