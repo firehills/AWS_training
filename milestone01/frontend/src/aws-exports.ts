6571b06a30870fad7fec0967924275eec152e7ab
Philip Hill <philip.hill@hidglobal.com>
2025-04-23 13:06:35 +0100

    link with created backend userpools

--- milestone01/frontend/src/aws-exports.ts
+++ milestone01/frontend/src/aws-exports.ts
@@ -1,6 +1,6 @@
 export const amplifyConfig = {
   aws_project_region: "us-east-1",
   aws_cognito_region: "us-east-1",
-  aws_user_pools_id: "us-east-1_eJg72xFLu",
-  aws_user_pools_web_client_id: "21h6qj5pd76u1rgp5k5ml76aks",
+  aws_user_pools_id: "us-east-1_y6k1G576B",
+  aws_user_pools_web_client_id: "7n1cg7j2c2mab6phkattirq4u2",
 };
